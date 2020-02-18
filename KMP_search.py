6a3d5b367901081641d8b4b0b8d72550498ac11d
kvartak2 <54905094+kvartak2@users.noreply.github.com>
2020-02-18 00:57:53 -0500

    added pattern matching function
    Pattern match function searches pattern to be searched in given string and returns the index in a string where pattern starts.

--- KMP_search.py
+++ KMP_search.py
@@ -4,7 +4,7 @@ import numpy as np
 def fun_lps(pattern_list,lps):
     i=0
     j=1
-    while(j<LEN):
+    while(j<find_pat_LEN):
         if(pattern_list[i]!=pattern_list[j]):
             if(i==0):
                 lps[j]=0
@@ -16,24 +16,42 @@ def fun_lps(pattern_list,lps):
             i=i+1
             j=j+1
     
-    print(lps)
-
+def fun_pat_match(pattern_list,find_pat_list,lps):   
+    i=0
+    j=0
+    while(i<pattern_LEN):
+        if(pattern_list[i]==find_pat_list[j]):
+            i=i+1
+            j=j+1
+        if(j==find_pat_LEN):
+            print("Pattern Found Index="+str(i-j))
+            j=lps[j-1]
+        elif(pattern_list[i]!=find_pat_list[j]):
+            if(j==0):
+                i=i+1
+            elif(j!=0):
+                j=lps[j-1]
 
 
 
 pattern="ABABC"
 pattern_list = list(disease)
+find_pat="ABC"
+find_pat_list=list(find_pat)
 
 
 
-LEN=len(pattern_list)
-lps=[0] * LEN
+pattern_LEN=len(pattern_list)
+find_pat_LEN=len(find_pat_list)
+lps=[0] * find_pat_LEN
 #lps=[]
 #print(LEN)
 #print(lps)
 
 
 fun_lps(pattern_list,lps)
+print(lps)
+fun_pat_match(pattern_list,find_pat_list,lps)
 
 
 #lps[0]=0
