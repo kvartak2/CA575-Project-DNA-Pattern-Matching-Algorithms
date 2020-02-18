b19eee88aaba82eb2d556fd91bdf1cd439746d14
kvartak2 <54905094+kvartak2@users.noreply.github.com>
2020-02-18 00:28:07 -0500

    Create lps[] array for KMP

---
+++ KMP_search.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+
+
+pattern="ABABC"
+pattern_list = list(disease)
+
+
+
+LEN=len(pattern_list)
+lps=[0] * LEN
+#lps=[]
+#print(LEN)
+#print(lps)
+
+
+i=0
+j=1
+#lps[0]=0
+
+
+while(j<LEN):
+    if(pattern_list[i]!=pattern_list[j]):
+        if(i==0):
+            lps[j]=0
+            j=j+1
+        else:
+            i=lps[i-1]
+    else:
+        lps[j]=i+1
+        i=i+1
+        j=j+1
+
+print(lps)
