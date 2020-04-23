720e4749006d90e1195806e965a004fe04888258
Kasturi Vartak <54905094+kvartak2@users.noreply.github.com>
2020-04-23 10:07:25 -0400

    Update Boyer_Moore.py

--- Boyer_Moore.py
+++ Boyer_Moore.py
@@ -1,6 +1,5 @@
 """
 Created on Wed Mar 18 03:05:41 2020
-
 @author: kasturivartak
 """
 import time
@@ -8,38 +7,13 @@ import time
 
 start = time.time()
 
-
-def search(DNA_seq, pat):
-    i=0
-    patLen=len(pat)
-    DNALen=len(DNA_seq)
-    
-
-    borderArray = [0] * (patLen + 1) 
-    shiftArray = [0] * (patLen + 1) 
-    
-    fullSuffixMatch_case1(shiftArray, borderArray, pat, patLen)
-    fullSuffixMatch_case2(shiftArray, borderArray, pat, patLen)
-    
-    
-    while(i <= DNALen - patLen):
-        j=patLen -1
-        while(j>=0 and pat[j] == DNA_seq[i+j]):
-            j=j-1
-        if(j<0):
-            print("\npattern found at position : ",i)
-            i = i +shiftArray[0]
-        else:
-            i = i + shiftArray[j+1]
-
-
-def fullSuffixMatch_case1(shiftArray, borderArray, pat, patLen): 
+def fullSuffixMatch_case1(shiftArray, borderArray, DNA_pattern, patLen): 
     i = patLen 
     j = patLen + 1
     borderArray[i] = j
     
     while(i > 0): 
-        while ((j <= patLen) and (pat[i - 1] != pat[j - 1])):
+        while ((j <= patLen) and (DNA_pattern[i - 1] != DNA_pattern[j - 1])):
             if(shiftArray[j]==0):
                 shiftArray[j] = j - i 
             j = borderArray[j] 
@@ -48,7 +22,7 @@ def fullSuffixMatch_case1(shiftArray, borderArray, pat, patLen):
         borderArray[i] = j 
     
 
-def fullSuffixMatch_case2(shiftArray, borderArray, pat, patLen): 
+def fullSuffixMatch_case2(shiftArray, borderArray, DNA_pattern, patLen): 
     j=borderArray[0]
     for i in range(patLen+1):
         if(shiftArray[i]==0):
@@ -63,10 +37,30 @@ DNA_seq=DNA_contents
 
 lamda_virus_f = open("lamda_virus.txt","r")
 lamda_virus_contents = lamda_virus_f.read()
-pat=lamda_virus_contents
+DNA_pattern=lamda_virus_contents
 
+i=0
+patLen=len(DNA_pattern)
+DNALen=len(DNA_seq)
+    
 
-search(DNA_seq, pat)
+borderArray = [0] * (patLen + 1) 
+shiftArray = [0] * (patLen + 1) 
+    
+fullSuffixMatch_case1(shiftArray, borderArray, DNA_pattern, patLen)
+print(borderArray)
+fullSuffixMatch_case2(shiftArray, borderArray, DNA_pattern, patLen)
+    
+    
+while(i <= DNALen - patLen):
+    j=patLen -1
+    while(j>=0 and DNA_pattern[j] == DNA_seq[i+j]):
+        j=j-1
+    if(j<0):
+        print("\npattern found at position : ",i)
+        i = i +shiftArray[0]
+    else:
+        i = i + shiftArray[j+1]
 
 
 lamda_virus_f.close()
